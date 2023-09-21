91542bfd1b52185132356a5a2860d81c2f1a22ba
Przemo <przemo06133@gmail.com>
2023-09-21 20:49:37 +0200

    Add consts

--- foundary-contract/script/HelperConfig.s.sol
+++ foundary-contract/script/HelperConfig.s.sol
@@ -7,28 +7,42 @@ import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+    uint32 public constant SEPOLIA_CHAIN_ID = 11155111;
+
+    address public constant SEPOLIA_ETH_USD =
+        0x694AA1769357215DE4FAC081bf1f309aDC325306;
+
     struct NetworkConfig {
         address priceFeed; // ETH/USD
     }
 
     constructor() {
-        if (block.chainid == 11155111) {
+        if (block.chainid == SEPOLIA_CHAIN_ID) {
             activeNetworkConfig = getSepoliaEthConfig();
         } else {
-            activeNetworkConfig = getEnvilEthConfig();
+            activeNetworkConfig = getOrCreateEnvilEthConfig();
         }
     }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         NetworkConfig memory sepoliaConfig = NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+            priceFeed: SEPOLIA_ETH_USD
         });
         return sepoliaConfig;
     }
 
-    function getEnvilEthConfig() public returns (NetworkConfig memory) {
+    function getOrCreateEnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
         vm.stopBroadcast();
 
         NetworkConfig memory anvilConfig = NetworkConfig({
